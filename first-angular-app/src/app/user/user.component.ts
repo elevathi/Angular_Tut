d83e6862bb81905472600957e5c08a4b64fcdf09
Gregor <28386827+elevathi@users.noreply.github.com>
2026-01-02 13:01:31 +0100

    input signals

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,19 +1,21 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, input, computed } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input() avatar !: string; //!: means definitely will be assigned
-  @Input() name !: string;
-  get imagePath() {
-    return 'assets/users/' + this.avatar;
-  }
-  onSelectUser() {
-  
-  }
-
+  // @Input({required:true}) avatar !: string; //!: means definitely will be assigned
+  // @Input({required:true}) name !: string;
+  avatar = input.required<string>();
+  name = input.required<string>();
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar();
+  });
+  // get imagePath() {
+  //   return 'assets/users/' + this.avatar;
+  // }
+  onSelectUser() {}
 }
