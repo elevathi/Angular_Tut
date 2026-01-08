1e160a6271071c65d7e05047a06e8ae772f458a1
Gregor <28386827+elevathi@users.noreply.github.com>
2026-01-08 17:09:59 +0100

    calculate event binding

--- 01-starting-project/src/app/user-input/user-input.component.ts
+++ 01-starting-project/src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -8,15 +8,23 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  calculate = output<{
+    initialInvestment: number;
+    duration: number;
+    expectedReturn: number;
+    annualInvestment: number;
+  }>();
+
   enteredInitialInvestment = '0';
   enteredAnnualInvestments = '0';
   enteredExpectedReturn = '5';
   enteredDuration = '10';
-  onSubmit(){
-    console.log("Submitted");
-    console.log(this.enteredInitialInvestment);
-    console.log(this.enteredAnnualInvestments);
-    console.log(this.enteredExpectedReturn);
-    console.log(this.enteredDuration);
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      duration: +this.enteredDuration,
+      expectedReturn:+this.enteredExpectedReturn,
+      annualInvestment:+this.enteredAnnualInvestments,
+    });
   }
 }
